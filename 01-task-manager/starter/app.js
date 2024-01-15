c2f26a5e85c8c8b1fb3f879633e983a021c41013
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-16 00:29:02 +0530

    setup routes

--- 01-task-manager/starter/app.js
+++ 01-task-manager/starter/app.js
@@ -3,4 +3,13 @@ const app = express()
 
 const port = 3000
 
+app.get('/hello',(req,res)=>{
+    res.send('Task Manager App')
+})
+
+app.get('api/v1/tasks')
+app.post('api/v1/tasks')
+app.get('api/v1/tasks/:id')
+app.patch('api/v1/tasks/:id')
+app.delete('api/v1/tasks/:id')
 app.listen(port, console.log(`Server is listening on ${port}...`))
