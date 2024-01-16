c9da154f303aaed344f1228d73414e8df6a5a143
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-16 22:21:35 +0530

    routes setup

--- 01-task-manager/starter/app.js
+++ 01-task-manager/starter/app.js
@@ -10,6 +10,8 @@ app.get('/hello',(req,res)=>{
 app.get('api/v1/tasks')
 app.post('api/v1/tasks')
 app.get('api/v1/tasks/:id')
-app.patch('api/v1/tasks/:id')
+app.patch('api/v1/tasks/:id') // option to app.put
 app.delete('api/v1/tasks/:id')
+
+
 app.listen(port, console.log(`Server is listening on ${port}...`))
