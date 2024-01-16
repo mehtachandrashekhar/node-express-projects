fddb31aa780bbaed86ef0e4c462e130c5f0e83f4
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-16 22:34:14 +0530

    tasks.js

--- 01-task-manager/starter/routes/tasks.js
+++ 01-task-manager/starter/routes/tasks.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 
 router.route('/').get((req,res)=>{
     res.send('all items')
 })
+
+module.exports = router
