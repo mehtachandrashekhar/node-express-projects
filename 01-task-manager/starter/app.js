45b72349e0a43dc1adc24c71d57b6a873c0700f0
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-16 22:34:02 +0530

    app.js

--- 01-task-manager/starter/app.js
+++ 01-task-manager/starter/app.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
 
-const port = 3000
+//middleware 
 
+app.use(express.json())
+
+//routes
 app.get('/hello',(req,res)=>{
     res.send('Task Manager App')
 })
 
-app.get('api/v1/tasks')
-app.post('api/v1/tasks')
-app.get('api/v1/tasks/:id')
-app.patch('api/v1/tasks/:id') // option to app.put
-app.delete('api/v1/tasks/:id')
+app.use('/api/v1/tasks', tasks)
 
+// app.get('api/v1/tasks')
+// app.post('api/v1/tasks')
+// app.get('api/v1/tasks/:id')
+// app.patch('api/v1/tasks/:id') // option to app.put
+// app.delete('api/v1/tasks/:id')
+
+const port = 3000
 
 app.listen(port, console.log(`Server is listening on ${port}...`))
