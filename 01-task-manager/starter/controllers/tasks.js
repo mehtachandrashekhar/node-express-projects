15f0f048e6cfc3e2a812303dc63e75c064378189
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-17 22:50:58 +0530

    controller.js

--- 01-task-manager/starter/controllers/tasks.js
+++ 01-task-manager/starter/controllers/tasks.js
@@ -2,4 +2,27 @@ const getAllTasks = (req, res) => {
     res.send('all items from the file')
 }
 
-module.exports = { getAllTasks }
+const createTask = (req, res) => {
+    res.send('Create Task')
+}
+
+const getTask = (req, res) => {
+    res.send('Get single Task')
+}
+
+const updateTask = (req, res) => {
+    res.send('Upate Task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Delete Task')
+}
+
+module.exports = { 
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+    
+ }
