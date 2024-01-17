444888323470d2f9724de09e00349427c233536f
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-17 23:00:12 +0530

    routes

--- 01-task-manager/starter/routes/tasks.js
+++ 01-task-manager/starter/routes/tasks.js
@@ -3,14 +3,8 @@ const app = express()
 const router = express.Router()
 const { getAllTasks,createTask,getTask,updateTask, deleteTask} = require('../controllers/tasks')
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createTask)
 
-router.route('/').get(getTask)
-
-router.route('/').get(createTask)
-
-router.route('/').get(updateTask)
-
-router.route('/').get(deleteTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router
