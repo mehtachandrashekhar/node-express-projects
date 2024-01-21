9a243cc314165ac46f7be9669912af0cfcc768cd
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-21 22:17:19 +0530

    db setup

--- 01-task-manager/starter/db/connect.js
+++ 01-task-manager/starter/db/connect.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose')
 const connectionString = 'mongodb+srv://Chandra_11:s6PAQmI3KVm11q2N@cluster11.aggl6wf.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose
-.connect(connectionString)
+.connect(connectionString,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true,
+})
 .then(() => { console.log('Connected to the Database') })
 .catch((err) => console.log(err))
