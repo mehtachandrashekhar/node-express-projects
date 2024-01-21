a088c39e246257feda72a654460a213f70e62c99
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-21 23:14:02 +0530

    Revert "db setup"
    This reverts commit 9a243cc314165ac46f7be9669912af0cfcc768cd.

--- 01-task-manager/starter/db/connect.js
+++ 01-task-manager/starter/db/connect.js
@@ -3,11 +3,6 @@ const mongoose = require('mongoose')
 const connectionString = 'mongodb+srv://Chandra_11:s6PAQmI3KVm11q2N@cluster11.aggl6wf.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose
-.connect(connectionString,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:false,
-    useUnifiedTopology:true,
-})
+.connect(connectionString)
 .then(() => { console.log('Connected to the Database') })
 .catch((err) => console.log(err))
