a04d5742902ce726ca070336c8f099518fed40c2
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-23 22:46:17 +0530

    connectDB setup

--- 01-task-manager/starter/app.js
+++ 01-task-manager/starter/app.js
@@ -1,7 +1,7 @@
-require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 //middleware 
 
@@ -22,4 +22,14 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
-app.listen(port, console.log(`Server is listening on ${port}...`))
+const start = async () => {
+    try {
+        await connectDB
+        app.listen(port, console.log(`Server is listening on ${port}...`))
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+start()
