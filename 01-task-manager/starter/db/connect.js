76ce05bf04631f68ab7cda6c6ab2ad80c57f1cf7
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-27 23:27:27 +0530

    mongoose URI setup

--- 01-task-manager/starter/db/connect.js
+++ 01-task-manager/starter/db/connect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster11.aggl6wf.mongodb.net/?retryWrites=true&w=majority`
 
 const connectDB = (url) => {
     return mongoose.connect(connectionString, {
