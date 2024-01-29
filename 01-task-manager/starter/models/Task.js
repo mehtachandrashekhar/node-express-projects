4706b1dadd1f6a13b1511ab50f2dfe877e465b6b
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-01-29 23:23:12 +0530

    name validation

--- 01-task-manager/starter/models/Task.js
+++ 01-task-manager/starter/models/Task.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name: String, completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'please provide a name'],
+        trim: true,
+        maxlenght: [20, 'name cannot be more than 20 characters']
+    },
+    completed: Boolean
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
