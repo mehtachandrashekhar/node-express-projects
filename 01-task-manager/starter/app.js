2e829eb185bfff40777a6ede1d29a0d0bdd9cfd0
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-03-07 21:31:03 +0530

    app.js update

--- 01-task-manager/starter/app.js
+++ 01-task-manager/starter/app.js
@@ -1,41 +1,33 @@
-const express = require('express')
-const app = express()
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// middleware
 
-//middleware 
+app.use(express.static('./public'));
+app.use(express.json());
 
-app.use(express.json())
+// routes
 
-//routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-})
+app.use('/api/v1/tasks', tasks);
 
-app.use('/api/v1/tasks', tasks)
-
-const port = 3000
-
-// app.get('api/v1/tasks')          -Get All Task
-// app.post('api/v1/tasks')         -Create Task
-// app.get('api/v1/tasks/:id')      -Get Task
-// app.patch('api/v1/tasks/:id')    -Update Task
-// app.delete('api/v1/tasks/:id')   -Delete Task
-
-
-// MONGO_URI is equal to connecion string in .env file
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on ${port}...`))
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
